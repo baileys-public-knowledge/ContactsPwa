9ca9277380966ce341bafc856bc85e5ecbabf81e
Bailey Miller <bjm19@pct.edu>
2019-06-04 09:10:02 -0400

    *Setup the basic layout for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +24,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     environment.production ?
         [] :
         [ AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot() ],
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
