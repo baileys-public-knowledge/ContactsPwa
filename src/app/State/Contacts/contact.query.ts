b96b5feefcdf008aa008ce69535f793910ff6773
Bailey Miller <bjm19@pct.edu>
2019-06-04 17:10:17 -0400

    Search bar works

--- src/app/State/Contacts/contact.query.ts
+++ src/app/State/Contacts/contact.query.ts
@@ -1,50 +1,32 @@
 import { Injectable } from '@angular/core';
-import { QueryEntity } from '@datorama/akita';
+import { QueryEntity, toBoolean } from '@datorama/akita';
 import { ContactStore, ContactState } from './contact.store';
 import { Contact } from './contact.model';
-import { filter } from 'minimatch';
-import { map } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactQuery extends QueryEntity<ContactState, Contact> {
 
-  public getAll$ = this.selectAll();
-
-  public ByName$(term: string)
-  {
-    if(!term)
-    {
-      return this.getAll$;
-    }else
-    {
-      return this.getAll$
-        .pipe(
-          map(contacts => 
-            contacts
-            .filter(contact => 
-              `${contact.FirstName} ${contact.LastName}`.includes(term)
-              )));
-    }
-  }
-
-  public ByEmail$(email: string)
-  {
-    if(!email)
-    {
-      return this.getAll$;
-    }else
-    {
-      return this.getAll$
-          .pipe(
-            map(contacts =>
-              contacts.filter(contact =>
-                  contact.Email.includes(email)
-                ))
-          );
-    }
-  }
+  public contactFilterTerm$ = this.select(state => state.ui.contactFilterTerm);
+  public hasContactFilterTerm$ = this.select(state => toBoolean(state.ui.contactFilterTerm != ''));
+  
+  public getAll$ = this.contactFilterTerm$
+    .pipe(
+      switchMap(
+        search => 
+        {
+          if(search === '')
+          {
+            return this.selectAll();
+          }else
+          {
+            return this.selectAll({filterBy: entity => `${entity.FirstName} ${entity.LastName}`.includes(search)});
+          }
+        }
+        )
+      );
 
   constructor(protected store: ContactStore) {
     super(store);
