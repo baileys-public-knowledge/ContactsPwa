e17e47ae7a31e23209540d93af413d5884c8fc03
Bailey Miller <bjm19@pct.edu>
2019-06-08 12:42:04 -0400

    Update notification and service

--- src/app/Services/update.service.ts
+++ src/app/Services/update.service.ts
@@ -1,15 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ApplicationRef } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material';
+import { first } from 'rxjs/operators';
+import { interval, concat } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
 
-  constructor(private updates: SwUpdate) {
+  constructor(private appRef: ApplicationRef, private updates: SwUpdate, private _snackBar: MatSnackBar) {
+
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+
+
+    appIsStable$.subscribe(x=>{
+      this.updates.checkForUpdate();
+    });
+
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+
+    everySixHoursOnceAppIsStable$.subscribe(x=> this.updates.checkForUpdate());
+
+
 
     this.updates.available.subscribe(event =>
       {
+        this.DisplayUpdateNotification();
         console.log(`Old: ${event.current} New: ${event.available}`);
       });
 
@@ -17,5 +36,42 @@ export class UpdateService {
       {
         console.log(`Old was: ${event.previous} New is ${event.current}`);
       });
+   }
+
+
+
+
+   private DisplayUpdateNotification()
+   {
+     // 
+     var updateNotification = this._snackBar.open("An Update is available", "Update", {
+       horizontalPosition: "left"
+     });
+
+
+     updateNotification.afterDismissed().subscribe(x=>{
+      if(!x.dismissedByAction)
+      {
+        // Sleep 10 seconds and show again
+        setTimeout(() => {
+          this.DisplayUpdateNotification();
+        }, 10*1000);
+      }
+     });
+
+
+     updateNotification.onAction().subscribe(()=>
+      {
+        // Perform the update
+        console.log("Update Triggered");
+        this.UpdateApplication();
+      });
+   }
+
+
+
+   public UpdateApplication()
+   {
+     document.location.reload();
    }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule, MatSnackBarModule } from '@angular/material';
 
 @NgModule({
   imports: [
@@ -11,7 +11,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatIconModule,
     MatListModule,
     MatFormFieldModule,
-    MatInputModule],
+    MatInputModule,
+    MatSnackBarModule],
   exports:
     [
       MatToolbarModule,
@@ -20,7 +21,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
       MatIconModule,
       MatListModule,
       MatFormFieldModule,
-      MatInputModule
+      MatInputModule,
+      MatSnackBarModule
     ]
 })
 export class MaterialModule { }
